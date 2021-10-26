1f65e6c0e8913d65fdf3175aa818fbcb9f9ee832
André <andremgmonteiro01@gmail.com>
2021-10-26 02:03:07 +0100

    feat: Added simple character
    Lucas added a simple character for testing

---
+++ Assets/Character test/AnimationTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+    private Animator _animator;
+    
+    [Range(0,1)] public float animatorBlend = 0;
+
+    [SerializeField] private GameObject _face;
+    [Range(0, 2)] public int faceIndex = 0;
+    [SerializeField] private Texture[] _faces;
+    [SerializeField] private bool _changeFace = false;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        _animator.SetFloat("MoveBlend", animatorBlend);
+        if (_changeFace)
+        {
+            _face.GetComponent<SkinnedMeshRenderer>().materials[0].mainTexture = _faces[faceIndex];
+            _changeFace = false;
+        }
+    }
+}
