832482c0e26926f58cdcec69d6b99730d93b849d
André <andremgmonteiro01@gmail.com>
2021-10-27 01:01:58 +0100

    feat: Added a new way to test multiple Controllers
    Added a new Player prefab so we could see the different gamepads in test.

--- Assets/Scripts/InputTesting.cs
+++ Assets/Scripts/InputTesting.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public struct Player
+{
+    public static int currentId = 0;
+    public GameObject prefab;
+    public Gamepad gamepad;
+    public int id;
+}
+
 public class InputTesting : MonoBehaviour
 {
-    private List<Gamepad> gamepads = new List<Gamepad>();
-    
+    private List<Player> players = new List<Player>();
+    [SerializeField] private GameObject playerPrefab;
+
+    private void Start()
+    {
+        foreach (Gamepad gamepad in Gamepad.all)
+        {
+            Player player = new Player
+            {
+                gamepad = gamepad,
+                prefab = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity),
+                id = Player.currentId++
+            };
+            
+            players.Add(player);
+        }
+    }
+
     private void Update()
     {
-        Gamepad gamepad = Gamepad.current;
-        
-        if (gamepads.Contains(gamepad)) return;
-        
-        gamepads.Add(gamepad);
-        Debug.Log($"Added gamepad {gamepad.name}");
+        foreach (Player player in players)
+        {
+            if (player.gamepad.leftStick.down.isPressed)
+            {
+                player.prefab.transform.position += Vector3.down * Time.deltaTime;
+            } 
+            else if (player.gamepad.leftStick.up.isPressed)
+            {
+                player.prefab.transform.position += Vector3.up * Time.deltaTime;
+            }
+            else if (player.gamepad.leftStick.left.isPressed)
+            {
+                player.prefab.transform.position += Vector3.left * Time.deltaTime;
+            }
+            else if (player.gamepad.leftStick.right.isPressed)
+            {
+                player.prefab.transform.position += Vector3.right * Time.deltaTime;
+            }
+        }
     }
 }
