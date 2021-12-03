70baa9440171b7d9ddb5c02afe297b88f973bcc0
Lucas <lucas_cmattos@hotmail.com.br>
2021-12-03 20:06:07 +0000

    feat: Added a char and animations
    Added the Flowey Character and animations for some minigames. Also updated AnimationTest script to test out the new animations. Also deleted some unnecessary textures. Also changed a quality setting for the skinning of the characters.

--- Assets/Scripts/AnimationTest.cs
+++ Assets/Scripts/AnimationTest.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class AnimationTest : MonoBehaviour
 {
     private Animator _animator;
-    
+
     [Range(0,1)] public float animatorBlend = 0;
     [SerializeField] private bool _jump = false;
+    [SerializeField] private bool _stun = false;
+    [SerializeField] private bool _crouch = false;
+    [SerializeField, Range(0,8)] private int _animationTestInt = 0;
     void Start()
     {
         _animator = GetComponent<Animator>();
@@ -17,5 +20,8 @@ public class AnimationTest : MonoBehaviour
     {
         _animator.SetFloat("MoveBlend", animatorBlend);
         _animator.SetBool("isJumping", _jump);
+        _animator.SetBool("isStunned", _stun);
+        _animator.SetBool("isCrouching", _crouch);
+        _animator.SetInteger("AnimationTestInt", _animationTestInt);
     }
 }
