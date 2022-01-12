53cde99f4cae4df55d16897cff50e18e2464e8a6
Lucas <lucas_cmattos@hotmail.com.br>
2022-01-12 16:36:40 +0000

    update to lobby ui

--- Assets/Scripts/UI/UICharacterSelection.cs
+++ Assets/Scripts/UI/UICharacterSelection.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UICharacterSelection : MonoBehaviour {
 
     public int uiCharacterIndex;
-    
 
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI playerText;
+
+    private void Start()
+    {
+        playerText.SetText("P"+(uiCharacterIndex+1));
+    }
     public void ChangeCharacter(GameObject character)
     {
         CharacterManager.Instance.uiCharacters[uiCharacterIndex].ChangeModel(character);
