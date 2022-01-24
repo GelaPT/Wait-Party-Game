ed7a17cadded363ca9128a86a3dd1c2332a6e2de
André <andremgmonteiro01@gmail.com>
2022-01-24 02:32:45 +0000

    Small update to lobby UI

--- Assets/Scripts/AnimationTest.cs
+++ Assets/Scripts/AnimationTest.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimationTest : MonoBehaviour
@@ -7,13 +5,14 @@ public class AnimationTest : MonoBehaviour
     private Animator _animator;
 
     [Range(0,1)] public float animatorBlend = 0;
-    [SerializeField] private bool _jump = false;
+    /*[SerializeField] private bool _jump = false;
     [SerializeField] private bool _stun = false;
     [SerializeField] private bool _crouch = false;
     [SerializeField] private bool _victory = false;
     [SerializeField] private bool _defeated = false;
     [SerializeField] private bool _caught = false;
-    [SerializeField, Range(0,8)] private int _animationTestInt = 0;
+    [SerializeField, Range(0,8)] private int _animationTestInt = 0;*/
+
     void Start()
     {
         _animator = GetComponent<Animator>();
@@ -22,12 +21,12 @@ public class AnimationTest : MonoBehaviour
     void Update()
     {
         _animator.SetLayerWeight(1, animatorBlend);
-        //_animator.SetBool("isJumping", _jump);
-        //_animator.SetBool("isStunned", _stun);
-        //_animator.SetBool("isCrouching", _crouch);
-        //_animator.SetBool("Victory", _victory);
-        //_animator.SetBool("Defeated", _defeated);
-        //_animator.SetBool("Caught", _caught);
-        //_animator.SetInteger("AnimationTestInt", _animationTestInt);
+        /*_animator.SetBool("isJumping", _jump);
+        _animator.SetBool("isStunned", _stun);
+        _animator.SetBool("isCrouching", _crouch);
+        _animator.SetBool("Victory", _victory);
+        _animator.SetBool("Defeated", _defeated);
+        _animator.SetBool("Caught", _caught);
+        _animator.SetInteger("AnimationTestInt", _animationTestInt);*/
     }
 }
