2f4b2febf9e958c42d6275955e49a85d040322cd
Lucas <lucas_cmattos@hotmail.com.br>
2022-01-29 20:21:44 +0000

    added back script

---
+++ Assets/Scripts/ForcePlay.cs
@@ -0,0 +1,27 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public static class ForcePlay
+{
+    static ForcePlay()
+    {
+        EditorApplication.playModeStateChanged += LoadDefaultScene;
+    }
+
+    static void LoadDefaultScene(PlayModeStateChange state)
+    {
+        if(state == PlayModeStateChange.ExitingEditMode)
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        }
+
+        if(state == PlayModeStateChange.EnteredPlayMode)
+        {
+            if (EditorSceneManager.GetActiveScene().buildIndex == 0) return;
+            EditorSceneManager.LoadScene(0);
+        }
+    }
+}
+#endif
